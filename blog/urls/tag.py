ce081964edc8a0900a5f05f308d178864c085ef4
グエン ヴァン ドゥック <nguyen.van.duc.26@shiftinc.jp>
2021-06-15 11:09:40 +0900

    add: form add tag

--- blog/urls/tag.py
+++ blog/urls/tag.py
@@ -4,5 +4,6 @@ from ..views import tags
 
 urlpatterns_tag = [
     path('tag/', tags.index, name='tag_index'),
-    path('tag/<int:pk>/', tags.detail, name='tag_detail')
+    path('tag/<int:pk>/', tags.detail, name='tag_detail'),
+    path('tag/create/', tags.create_tag, name='tag_create')
 ]
--- blog/views/tags.py
+++ blog/views/tags.py
@@ -1,6 +1,10 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render 
+from django.urls import reverse
+
 
 from ..models import Tag
+from ..forms import TagForm
 
 def index(request):
     tags = Tag.objects.all()
@@ -11,3 +15,14 @@ def detail(request, pk):
     tag = Tag.objects.get(pk=pk)
 
     return render(request, 'tag/detail.html', {"tag": tag})
+
+def create_tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tag_index'))
+    else:
+        form = TagForm()
+
+    return render(request, 'tag/new.html', {'form': form})
